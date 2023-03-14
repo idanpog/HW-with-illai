d988e85cdb95a96c1e55f3bc5cf487e378b8970e
idanpog <40134558+idanpog@users.noreply.github.com>
2023-03-14 22:19:28 +0200

    initial attempts - nothing functioning yet

--- NLP/NLProject/datamanager.py
+++ NLP/NLProject/datamanager.py
@@ -3,10 +3,7 @@ this file is responsible for the data management of the application
 loads the data, handles the preprocessing and the data augmentation
 """
 
-
-
-
-def load_train():
+def load_dataset(path):
     """
     loads the data from the given path
     returns a list of tuples of the form (paragraph, translated)
@@ -14,11 +11,23 @@ def load_train():
     :param path:
     :return:
     """
-    with open('train.labeled', 'r+', encoding='iso-8859-15') as f:
+    with open(path, 'r+', encoding='iso-8859-15') as f:
         lines = f.readlines()
     lines = "".join(lines)
     lines = lines.split("German:\n")
     a = [line.split("English:\n") for line in lines if line != ""]
     return [tuple(x) for x in a]
 
-a = load_train()
+
+def check_max_length(path):
+    data = load_dataset(path)
+    print(max([len(x[0]) for x in data]))
+
+
+def preprocess_function(data, tokenizer):
+    prefix = 'Translate German to English: '
+
+    inputs = [prefix + par[0] for par in data]
+    targets = [par[1] for par in data]
+    model_inputs = tokenizer(inputs, text_target=targets, max_length=1500, truncation=True)
+    return model_inputs
--- NLP/NLProject/main.py
+++ NLP/NLProject/main.py
@@ -1,26 +1,97 @@
-# This is a sample Python script.
+from transformers import (
+    T5Tokenizer,
+    T5ForConditionalGeneration,
+    DataCollatorForSeq2Seq,
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer
+)
+from datamanager import load_dataset, preprocess_function
+import datasets
+import torch
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-from transformers import T5Tokenizer, T5ForConditionalGeneration, DataCollatorForSeq2Seq
-# from transformers import T5Tokenizer, T5Model
-
-tokenizer = T5Tokenizer.from_pretrained("t5-large")
-model = T5ForConditionalGeneration.from_pretrained("t5-large")
-
+tokenizer = T5Tokenizer.from_pretrained("t5-base")
+model = T5ForConditionalGeneration.from_pretrained("t5-base")
 collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
 
-text = 'Leider ist die konventionelle Analyse zum Zusammenbruch von Lehman Brothers Wunschdenken.'
-text = 'Unfortunately, the conventional post-mortem on Lehman Brothers is wishful thinking.'
-input_ids = tokenizer(
-    'Translate English to German:' + text, return_tensors="pt").input_ids  # Batch size 1
-labels = tokenizer('Unfortunately, the conventional post-mortem on Lehman Brothers is wishful thinking.', return_tensors='pt').input_ids
-# loss = model(input_ids=input_ids, labels=labels).loss
-# forward pass
-# loss = model(input_ids=input_ids, labels=labels)
-# last_hidden_states = outputs.last_hidden_state
-outputs = model.generate(input_ids)
-decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(decoded)
+metric = datasets.load_metric('sacrebleu')
+
+
+def postprocess_text(preds, labels):
+    preds = [pred.strip() for pred in preds]
+    labels = [[label.strip()] for label in labels]
+
+    return preds, labels
+
+
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+    result = {"bleu": result["score"]}
+
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
+    result["gen_len"] = np.mean(prediction_lens)
+    result = {k: round(v, 4) for k, v in result.items()}
+    return result
+
+
+class TokenizedDataset(torch.utils.data.Dataset):
+    def __init__(self, inputs, targets):
+        self.inputs = inputs
+        self.targets = targets
+
+    def __len__(self):
+        return len(self.targets)
+
+    def __getitem__(self, index):
+        input_ids = torch.tensor(self.inputs[index]).squeeze()
+        target_ids = torch.tensor(self.targets[index]).squeeze()
+
+        return {"input_ids": input_ids, "labels": target_ids}
+
+
+def create_dataset(path):
+    data = load_dataset(path)
+    tokenized = preprocess_function(data, tokenizer)
+    inputs = tokenized.data['input_ids']
+    targets = tokenized.data['labels']
+    return TokenizedDataset(inputs, targets)
+
+
+train_dataset = create_dataset('train.labeled')
+test_dataset = create_dataset('val.labeled')
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="no_modifiers_model",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    weight_decay=0.01,
+    save_total_limit=3,
+    num_train_epochs=2,
+    predict_with_generate=True,
+    fp16=True,
+    push_to_hub=False,
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
+    tokenizer=tokenizer,
+    data_collator=collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
